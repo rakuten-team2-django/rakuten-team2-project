6f5d10337932a9406458fff99f794b2ebebf6128
yuki-akiba-jp <rabuafe@gmail.com>
2023-09-26 16:14:02 +0900

    first commit

---
+++ reasonable_recommendation_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReasonableRecommendationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reasonable_recommendation_app'
