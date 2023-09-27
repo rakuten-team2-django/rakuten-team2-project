8a23e08f4769d138b8d1bd09778856a8b224c18b
yuki-akiba-jp <rabuafe@gmail.com>
2023-09-27 09:43:57 +0900

    register User model in admin.py

--- reasonable_recommendation_app/models.py
+++ reasonable_recommendation_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
+
+
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-
-
-
     email = models.CharField(max_length=200)
     birthday = models.DateField()
 
