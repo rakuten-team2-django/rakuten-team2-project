8368d8264a63630c7815d4c7270424d98f31101d
yuki-akiba-jp <rabuafe@gmail.com>
2023-09-27 10:06:30 +0900

    fix problem related to html structure

--- reasonable_recommendation_app/views.py
+++ reasonable_recommendation_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'reasonable_recommendation_app/home.html', {})
 
 def test_koya(request):
     return render(request, 'test_koya.html', {})
@@ -14,5 +15,5 @@ def test_bibek(request):
     return render(request, 'test_bibek.html', {})
 
 def test_akiba(request):
-    return render(request, 'test_bibek.html', {})
+    return render(request, 'reasonable_recommendation_app/test_akiba.html', {})
 
