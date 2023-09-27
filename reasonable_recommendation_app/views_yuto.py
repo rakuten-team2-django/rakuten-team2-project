4f6dd539000b72779b5eaaffe9e3fff29814084f
nakamurayuto <82076440+YutoNakamura1030@users.noreply.github.com>
2023-09-27 16:45:54 +0900

    fix views_yuto.py

--- reasonable_recommendation_app/views_yuto.py
+++ reasonable_recommendation_app/views_yuto.py
@@ -3,6 +3,10 @@ from django.views.generic import TemplateView
 from django.views.generic import ListView
 from .models import User, Item # TODO
 import datetime
+from decimal import Decimal, ROUND_HALF_UP
+
+DISCOUNTRATE = 0.10 # 0.05
+TESTUSER = 1
 
 class DiscountApplier(ListView):
     # TODO: Change name
@@ -11,28 +15,30 @@ class DiscountApplier(ListView):
     # TODO: Change name
     template_name = 'reasonable_recommendation_app/discounts_items.html'
 
-    paginate_by = 10
+    # paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        return context
-        raise NotImplementedError
-        # fetch user information from database
-        user_id = self.request.POST.get('user_id', '') # TODO
+        user_id = self.request.POST.get('user_id', TESTUSER) # TODO
         user_info = User.objects.get(id=user_id)
-        for item in DiscountApplier.model:
-            
+        
+        if check_age(user_info):
+            for item in context['item_list']:
+                item = apply_discount(item)
+                item['discount_rate'] = DISCOUNTRATE
+        
         return context
     
 def apply_discount(item):
-    DISCOUNTRATE = 0.10 # 0.05
-    price = item.price * DISCOUNTRATE
-    return price
+    item.price = item.price * (1 - DISCOUNTRATE)
+    item.price = Decimal(item.price).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
+    return item
 
 def check_age(user):
     AGEMIN = 20
     AGEMAX = 30
     age = (int(datetime.date.today().strftime("%Y%m%d")) - int(user.birthday.strftime("%Y%m%d"))) // 10000
+    
     if AGEMIN <= int(age) < AGEMAX:
         return True
     else:
