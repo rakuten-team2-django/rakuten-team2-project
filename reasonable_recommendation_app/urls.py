a43e605a1f6c4d958af657a4b21d7bd8f5aee08c
Bibek Shrestha <77220573+bibekshrestha22@users.noreply.github.com>
2023-09-28 15:38:50 +0900

    this commit was made on 3-39 28Th sept.

--- reasonable_recommendation_app/urls.py
+++ reasonable_recommendation_app/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-
-from .views import home, test_akiba, test_bibek, test_koya, test_yuto
+from .views import home, test_akiba, test_bibek, test_koya, test_yuto,rep_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name='home'),
     path('test_koya/',test_koya, name='test_koya'),
     path('test_yuto/',test_yuto, name='test_yuto'),
-    path('test_bibek/',test_bibek, name='test_bibek'),
+    path('disc_items/',test_bibek, name='fetch_least_sold_items'),
     path('test_akiba/',test_akiba, name='test_akiba'),
+    path('database_data/',test_bibek,name="database_data")
 ]
