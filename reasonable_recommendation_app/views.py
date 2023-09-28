b3ed6bd11d1b5febef0e8b01fc8735e152b44c8a
稲吉宏哉 <inayoshikoya@inayoshihiroshinarinoMacBook-Air.local>
2023-09-28 15:58:34 +0900

    create apply ranking button

--- reasonable_recommendation_app/views.py
+++ reasonable_recommendation_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.hashers import check_password, make_password
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from .koya_models import ResultItemsModel
 
 def home(request):
     print(request.user)
@@ -17,31 +18,61 @@ def home(request):
     context = {'user': user}
     return render(request, 'reasonable_recommendation_app/home.html', context)
 
-class test_koya(TemplateView):
+class test_koya_Search(TemplateView):
     def __init__(self):
         self.template_name = "reasonable_recommendation_app/test_koya.html"
         self.search_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
         self.ranking_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
-     
-    def get_context_data(self, **kwargs: any) -> dict[str, any]:
-        context_data = super().get_context_data(**kwargs)
-        context_data["test"] = "This is test Message"
-        data = requests.get("https://app.rakuten.co.jp/services/api/Product/Search/20170426?format=json&keyword=%E3%82%B7%E3%83%A3%E3%83%B3%E3%83%97%E3%83%BC&applicationId=1086392607264524220%20").json()
-        product_list = []
-        for product in data["Products"]:
-            product_list.append(product["Product"]["productName"])
-        context_data["product_list"] = product_list
-        return context_data
     
-    def fetch_all_page_items(self, rakutenAPI_url, num_page,request=None):
+    def post(self, request, *args, **kwargs):
+        result_item_list = test_koya_APIListOperations().fetch_all_page_items(self.search_url, 20, request)
+        result_item_list_sorted = test_koya_APIListOperations.bubble_sort_result_item_list_ascending_price(result_item_list)
+        ResultItemsModel.objects.all().delete()
+        for result_item in result_item_list_sorted:
+            ResultItemsModel.objects.create(item_name=result_item.item_name, item_price=result_item.item_price, item_code=result_item.item_code, image_urls=result_item.image_urls, ranking=result_item.ranking)
+        context = {"result_item_list": result_item_list_sorted}
+        return super().render_to_response(context)
+    
+class test_koya_RankingSearch(TemplateView):
+    def __init__(self):
+        self.template_name = "reasonable_recommendation_app/test_koya.html"
+        self.search_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
+        self.ranking_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
+    
+    def add_ranking_to_result_item_list(self, result_item_list):
+        ranking_list = test_koya_APIListOperations.fetch_all_page_items(self.ranking_url, 10)
+        result_item_list_tmp = result_item_list.copy()
+        #検索結果のアイテムリストにランキング情報を付与する。
+        for result_item in result_item_list_tmp:
+            for i in range(len(ranking_list)):
+                if result_item.item_code == ranking_list[i].item_code:
+                    result_item.ranking = i
+        
+        #ランキングがつかなかったアイテムを削除
+        result_item_list_added_ranking = [item for item in result_item_list_tmp if item.ranking != None]
+        return result_item_list_added_ranking
+    
+    def get(self, request, *args, **kwargs):
+        result_item_list = list(ResultItemsModel.objects.all())
+        print(result_item_list[0].item_name)
+        result_item_list_added_ranking = self.add_ranking_to_result_item_list(result_item_list)
+        context = {"result_item_list": result_item_list_added_ranking}
+        return super().render_to_response(context)
+    
+class test_koya_APIListOperations:
+    
+    @staticmethod
+    def fetch_all_page_items(rakutenAPI_url, num_page,request=None):
+        search_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
+        ranking_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
         all_res_data = []
         for i in range(1,num_page+1):
-            if rakutenAPI_url == self.search_url:
+            if rakutenAPI_url == search_url:
                 params = {"applicationId" : "1086392607264524220",
                             "keyword" : request.POST["keyword"],
                             "format" : "json",
                             "page" :i}   
-            elif rakutenAPI_url == self.ranking_url:
+            elif rakutenAPI_url == ranking_url:
                 params = {"applicationId" : "1086392607264524220",
                             "age": 20,
                             "format" : "json",
@@ -54,65 +85,20 @@ class test_koya(TemplateView):
             result_item = test_koya_ResultItem(item["Item"]["itemName"], item["Item"]["itemPrice"], item["Item"]["itemCode"], item["Item"]["mediumImageUrls"])
             result_item_list.append(result_item)
         return result_item_list
-   
-    def add_ranking_to_result_item_list(self, result_item_list):
-        ranking_list = self.fetch_all_page_items(self.ranking_url, 10)
-        result_item_list_tmp = result_item_list.copy()
-        #検索結果のアイテムリストにランキング情報を付与する。
-        for result_item in result_item_list_tmp:
-            for i in range(len(ranking_list)):
-                if result_item.item_code == ranking_list[i].item_code:
-                    result_item.ranking = i
-        
-        #ランキングがつかなかったアイテムを削除
-        result_item_list_added_ranking = [item for item in result_item_list_tmp if item.ranking != None]
-        return result_item_list_added_ranking
     
-    def bubble_sort_result_item_list_ascending_price(self, result_item_list_tmp):
+    @staticmethod
+    def bubble_sort_result_item_list_ascending_price(result_item_list_tmp):
         result_item_list_tmp = result_item_list_tmp.copy()
         for i in range(len(result_item_list_tmp)):
             for j in range(len(result_item_list_tmp) - i -1):
-                if result_item_list_tmp[j].price > result_item_list_tmp[j+1].price: #左の方が大きい場合
+                if result_item_list_tmp[j].item_price > result_item_list_tmp[j+1].item_price: #左の方が大きい場合
                     result_item_list_tmp[j], result_item_list_tmp[j+1] = result_item_list_tmp[j+1], result_item_list_tmp[j] #前後入れ替え
         return result_item_list_tmp
-    """
-    def add_images(result_item_list):
-        result_item_list_tmp = result_item_list.copy()
-        for result_item in result_item_list_tmp:
-            for image_url in result_item.image_urls:
-                image = requests.get(image_url)
-                result_item.images.append(image)
-        return result_item_list_tmp
-    """
-
-
-    
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return super().render_to_response(context)
-    
-    def post(self, request, *args, **kwargs):
-        """
-        params = {"applicationId" : "1086392607264524220",
-                  "keyword" : request.POST["keyword"],
-                  "format" : "json"}   
-        res_data = requests.get("https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601", params).json()
-        result_item_list = []
-        for item in res_data["Items"]:
-            result_item = test_koya_ResultItem(item["Item"]["itemName"], item["Item"]["itemPrice"])
-            result_item_list.append(result_item)
-        """
-        result_item_list = self.fetch_all_page_items(self.search_url, 10, request)
-        result_item_list_added_ranking = self.add_ranking_to_result_item_list(result_item_list)
-        result_item_list_added_ranking_sorted = self.bubble_sort_result_item_list_ascending_price(result_item_list_added_ranking)
-        print(result_item_list_added_ranking_sorted[0].image_urls)
-        context = {"result_item_list": result_item_list_added_ranking_sorted}
-        return super().render_to_response(context)
     
 class test_koya_ResultItem:
     def __init__(self, name, price, item_code, image_urls):
-        self.name = name
-        self.price = price
+        self.item_name = name
+        self.item_price = price
         self.item_code = item_code
         self.image_urls = []
         for image_url in image_urls:
