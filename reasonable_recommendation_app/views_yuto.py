a4409b66c549d492db31004e4d15e74e1efa4bc9
nakamurayuto <82076440+YutoNakamura1030@users.noreply.github.com>
2023-10-12 18:54:25 +0900

    fix views yuto

--- reasonable_recommendation_app/views_yuto.py
+++ reasonable_recommendation_app/views_yuto.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import ListView
-from .models import User, Item # TODO
+from .models import User, Discounted_Items # TODO
 import datetime
 from decimal import Decimal, ROUND_HALF_UP
 
 DISCOUNTRATE = 0.10 # 0.05
 TESTUSER = 1
+AGEMIN = 20
+AGEMAX = 30
 
 class DiscountApplier(ListView):
     # TODO: Change name
-    model = Item
+    model = Discounted_Items
 
     # TODO: Change name
     template_name = 'reasonable_recommendation_app/test_yuto.html'
@@ -23,10 +25,10 @@ class DiscountApplier(ListView):
         user_info = User.objects.get(id=user_id)
         
         if check_age(user_info):
-            for item in context['item_list']:
+            for item in context['object_list']:
                 item = apply_discount(item)
                 item.discount_rate = DISCOUNTRATE
-        
+
         return context
     
 def apply_discount(item):
@@ -35,12 +37,9 @@ def apply_discount(item):
     return item
 
 def check_age(user):
-    AGEMIN = 20
-    AGEMAX = 30
     age = (int(datetime.date.today().strftime("%Y%m%d")) - int(user.birthday.strftime("%Y%m%d"))) // 10000
     
     if AGEMIN <= int(age) < AGEMAX:
         return True
     else:
         return False
-
