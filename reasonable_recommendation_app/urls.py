48c72762c610607c4ab2f7a8630700619958d39d
稲吉宏哉 <inayoshikoya@inayoshihiroshinarinoMacBook-Air.local>
2023-10-12 18:33:20 +0900

    home画面を修正

--- reasonable_recommendation_app/urls.py
+++ reasonable_recommendation_app/urls.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 from .auth import user_login, user_logout, user_signup
-from .views import home, test_akiba, test_bibek, test_koya_Search_Reasonable, test_koya_RankingSearch,test_koya_Home, test_yuto
+from .views import test_akiba, test_bibek, Search_Reasonable, RankingSearch, Home, test_yuto
 
 app_name = 'reasonable_recommendation_app'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='home'),
-    path('test_koya/search_reasonable/<int:page>',test_koya_Search_Reasonable.as_view(), name='test_koya/search_reasonable'),
-    path('test_koya/ranking_search',test_koya_RankingSearch.as_view(), name='test_koya/ranking_search'),
-    path('test_koya/home',test_koya_Home.as_view(), name='test_koya/home'),
+    path('search_reasonable/<int:page>',Search_Reasonable.as_view(), name='search_reasonable'),
+    path('ranking_search/',RankingSearch.as_view(), name='ranking_search'),
+    path('home/',Home.as_view(), name='home'),
     path('test_yuto/',test_yuto, name='test_yuto'),
     path('disc_items/',test_bibek, name='fetch_least_sold_items'),
     path('test_akiba/',test_akiba, name='test_akiba'),
