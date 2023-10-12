2d5b92d2f803f2430625bf1c37c9ec2ccf6368e4
nakamurayuto <82076440+YutoNakamura1030@users.noreply.github.com>
2023-10-12 20:31:00 +0900

    fix url.py for test_yuto

--- reasonable_recommendation_app/urls.py
+++ reasonable_recommendation_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from .auth import user_login, user_logout, user_signup
 from .views import test_akiba, test_bibek, Search_Reasonable, RankingSearch, Home, test_yuto
+from .views_yuto import DiscountApplier
 
 app_name = 'reasonable_recommendation_app'
 urlpatterns = [
@@ -10,7 +11,7 @@ urlpatterns = [
     path('search_reasonable/<int:page>',Search_Reasonable.as_view(), name='search_reasonable'),
     path('ranking_search/',RankingSearch.as_view(), name='ranking_search'),
     path('home/',Home.as_view(), name='home'),
-    path('test_yuto/',test_yuto, name='test_yuto'),
+    path('test_yuto/', DiscountApplier.as_view(), name='test_yuto'),
     path('disc_items/',test_bibek, name='fetch_least_sold_items'),
     path('test_akiba/',test_akiba, name='test_akiba'),
     path('signup/', user_signup, name='signup'),
